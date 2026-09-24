5715979cd2d5ac0fadc6771b5928da7b2611ca7e
yaseraliraqi48-web <yaser.aliraqi48@gmail.com>
2026-09-25 02:06:01 +0300

    Add service worker with install and activate events

---
+++ Sw.js
@@ -0,0 +1,26 @@
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("notificationclick", event => {
+  event.notification.close();
+
+  event.waitUntil(
+    clients.matchAll({ type: "window", includeUncontrolled: true })
+      .then(clientList => {
+        for (const client of clientList) {
+          if ("focus" in client) {
+            return client.focus();
+          }
+        }
+
+        if (clients.openWindow) {
+          return clients.openWindow("/");
+        }
+      })
+  );
+});
