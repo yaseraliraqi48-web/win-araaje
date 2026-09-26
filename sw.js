f9a6357d6d2df67a6ae5706e4b223a7bd71a5114
yaseraliraqi48-web <yaser.aliraqi48@gmail.com>
2026-09-26 03:45:48 +0300

    Refactor service worker for caching and asset management
    Updated service worker to use a new cache name and manage assets.

--- sw.js
+++ sw.js
@@ -1,4 +1,30 @@
-const CACHE_NAME = "wen-araje-v1";
-self.addEventListener("install", e => { self.skipWaiting(); });
-self.addEventListener("activate", e => { self.clients.claim(); });
-self.addEventListener("fetch", e => { e.respondWith(fetch(e.request).catch(()=> caches.match(e.request))); });
+const CACHE = "win-araje-ultimate-v7";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
